92880e4a861db3c8b65b700f287c961f3f504eb6
Babak Nodoust <alive2212@gmail.com>
2020-06-16 20:01:37 +0430

    clean trans method and put some text into readme for localization

---
+++ src/LaravelSmartRestful.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace Alive2212\LaravelSmartRestful;
+
+
+class LaravelSmartRestful
+{
+
+}
--- src/LaravelSmartRestfulServiceProvider.php
+++ src/LaravelSmartRestfulServiceProvider.php
@@ -13,36 +13,17 @@ class LaravelSmartRestfulServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-         $this->loadTranslationsFrom(resource_path('lang/vendor/alive2212'), 'laravel_smart_restful');
-        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'alive2212');
-//        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-//        $this->loadRoutesFrom(__DIR__.'/routes.php');
+        $this->loadTranslationsFrom(resource_path('lang/vendor/alive2212'),
+            'laravel-smart-restful');
 
         // Publishing is only necessary when using the CLI.
         if ($this->app->runningInConsole()) {
 
-            // Publishing the configuration file.
-//            $this->publishes([
-//                __DIR__.'/../config/laravelwalletservice.php' => config_path('laravelwalletservice.php'),
-//            ], 'laravelwalletservice.config');
-
-            // Publishing the views.
-            /*$this->publishes([
-                __DIR__.'/../resources/views' => base_path('resources/views/vendor/alive2212'),
-            ], 'laravelwalletservice.views');*/
-
-            // Publishing assets.
-            /*$this->publishes([
-                __DIR__.'/../resources/assets' => public_path('vendor/alive2212'),
-            ], 'laravelwalletservice.views');*/
-
             // Publishing the translation files.
             $this->publishes([
                 __DIR__.'/../resources/lang/' => resource_path('lang/vendor/alive2212'),
-            ], 'laravel_smart_restful.lang');
+            ], 'laravel-smart-restful.lang');
 
-            // Registering package commands.
-            // $this->commands([]);
         }
     }
 
@@ -53,6 +34,10 @@ class LaravelSmartRestfulServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // Register the service the package provides.
+        $this->app->singleton('laravel-smart-restful', function ($app) {
+            return new LaravelSmartRestful;
+        });
+
     }
 }
